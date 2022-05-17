74a0af0a426ccce682caad09fb3e04609c576401
Iv@n_NEEkot1K <92731967+SamBara127@users.noreply.github.com>
2022-05-17 18:43:33 +0700

    новый файл:    .idea/.gitignore 	новый файл:    .idea/YouTube Downloader.iml 	новый файл:    .idea/inspectionProfiles/profiles_settings.xml 	новый файл:    .idea/misc.xml 	новый файл:    .idea/modules.xml 	новый файл:    main.py 	новый файл:    projectube/apptube/__init__.py 	новый файл:    projectube/apptube/__pycache__/__init__.cpython-38.pyc 	новый файл:    projectube/apptube/__pycache__/admin.cpython-38.pyc 	новый файл:    projectube/apptube/__pycache__/apps.cpython-38.pyc 	новый файл:    projectube/apptube/__pycache__/models.cpython-38.pyc 	новый файл:    projectube/apptube/__pycache__/urls.cpython-38.pyc 	новый файл:    projectube/apptube/__pycache__/views.cpython-38.pyc 	новый файл:    projectube/apptube/admin.py 	новый файл:    projectube/apptube/apps.py 	новый файл:    projectube/apptube/migrations/__init__.py 	новый файл:    projectube/apptube/migrations/__pycache__/__init__.cpython-38.pyc 	новый файл:    projectube/apptube/models.py 	новый файл:    projectube/apptube/templates/ytb_main.html 	новый файл:    projectube/apptube/tests.py 	новый файл:    projectube/apptube/urls.py 	новый файл:    projectube/apptube/views.py 	новый файл:    projectube/db.sqlite3 	новый файл:    projectube/manage.py 	новый файл:    projectube/projectube/__init__.py 	новый файл:    projectube/projectube/__pycache__/__init__.cpython-38.pyc 	новый файл:    projectube/projectube/__pycache__/settings.cpython-38.pyc 	новый файл:    projectube/projectube/__pycache__/urls.cpython-38.pyc 	новый файл:    projectube/projectube/__pycache__/wsgi.cpython-38.pyc 	новый файл:    projectube/projectube/asgi.py 	новый файл:    projectube/projectube/settings.py 	новый файл:    projectube/projectube/urls.py 	новый файл:    projectube/projectube/wsgi.py 	новый файл:    projectube/static_files/head.png

---
+++ projectube/apptube/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApptubeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apptube'
---
+++ projectube/apptube/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect
+
+
+def ytb_dw(request):
+    return render(request, 'ytb_main.html')
+
