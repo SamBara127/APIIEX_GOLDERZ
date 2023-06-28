872838fc4da21480faa7743ae11230d1bcd6eea0
Iv@n_NEEkot1K <92731967+SamBara127@users.noreply.github.com>
2023-06-28 13:26:50 +0700

    ADD_CUDA

--- projectube/apptube/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApptubeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apptube'
--- projectube/apptube/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, redirect
-
-
-def ytb_dw(request):
-    return render(request, 'ytb_main.html')
-
